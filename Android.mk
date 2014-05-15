4b376888321d394db01f2c3dd44b2fb53e65b2d9
Simon MacDonald <simon.macdonald@gmail.com>
2014-05-15 16:49:28 -0400

    RM #1070: Implement SSL for SSCM websockets

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := libwebsocket:autobahn-android-0.5.2-SNAPSHOT.jar
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := libwebsocket:androidasync-1.2.5.jar
 include $(BUILD_MULTI_PREBUILT)
