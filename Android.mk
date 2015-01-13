423ff65e72eeac4994878f50710aeda8ae70ed35
Andrew Thompson <andrew.thompson@live.ca>
2015-01-13 10:57:37 -0500

    Updating async android websocket library to 2.0.5.

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := libwebsocket:androidasync-1.2.5.jar
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := libwebsocket:androidasync-2.0.5.jar
 include $(BUILD_MULTI_PREBUILT)
